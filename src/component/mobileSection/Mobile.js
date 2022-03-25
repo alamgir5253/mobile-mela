51cbbbace250368b54f50073552c61ea674bd19a
alamgir5253 <alamgir5253@gmail.com>
2022-03-26 00:37:02 +0600

    icon added

--- src/component/mobileSection/Mobile.js
+++ src/component/mobileSection/Mobile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Mobile.css'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const Mobile = ({product}) => {
     const { name, price, img} = product
@@ -9,6 +10,10 @@ const Mobile = ({product}) => {
             <img src={img} alt="" />
             <h3>name: {name}</h3>
             <p>price: {price}</p>
+            <div className='addToCard'>
+                <button>add to card <AiOutlineShoppingCart className='icon' /></button>
+                <p></p>
+            </div>
             </div>
         </div>
     );
